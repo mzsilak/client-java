602652bc1a2283f391a894b501489e598457d7fa
Mario Zsilak <mario.zsilak@gmail.com>
2021-03-18 10:03:19 +0100

    more error messages

--- eisenstadt-demo-car/src/main/java/eu/arrowhead/client/car/ElectricCarApplication.java
+++ eisenstadt-demo-car/src/main/java/eu/arrowhead/client/car/ElectricCarApplication.java
@@ -23,14 +23,9 @@ import eu.arrowhead.demo.grovepi.ControllableLed;
 import eu.arrowhead.demo.grovepi.GroveButtonObserver;
 import eu.arrowhead.demo.onboarding.ArrowheadHandler;
 import eu.arrowhead.demo.onboarding.HttpClient;
-import eu.arrowhead.demo.ssl.SSLException;
 import eu.arrowhead.demo.utils.IpUtilities;
 import eu.arrowhead.demo.web.HttpServer;
 import java.io.IOException;
-import java.security.KeyStoreException;
-import java.security.NoSuchAlgorithmException;
-import java.security.cert.CertificateException;
-import java.security.spec.InvalidKeySpecException;
 import java.time.ZonedDateTime;
 import java.util.HashMap;
 import java.util.List;
@@ -51,194 +46,202 @@ import org.springframework.web.util.UriComponents;
 @Service
 public class ElectricCarApplication {
 
-    private final Logger logger = LogManager.getLogger();
-    private final AtomicBoolean onboarded = new AtomicBoolean(false);
-    private final ApplicationEventPublisher applicationEventPublisher;
-    private final HttpServer httpServer;
+  private final Logger logger = LogManager.getLogger();
+  private final AtomicBoolean onboarded = new AtomicBoolean(false);
+  private final ApplicationEventPublisher applicationEventPublisher;
+  private final HttpServer httpServer;
 
-    private final ArrowheadHandler onboardingHandler;
-    private final HttpClient httpClient;
-    private final String commonName;
-    private final String rfid;
+  private final ArrowheadHandler onboardingHandler;
+  private final HttpClient httpClient;
+  private final String commonName;
+  private final String rfid;
 
-    private final ControllableLed green;
-    private final ControllableLed red;
+  private final ControllableLed green;
+  private final ControllableLed red;
 
-    private final String ipAddress;
-    private final String macAddress;
-    private final String validity;
-    private final int port;
+  private final String ipAddress;
+  private final String macAddress;
+  private final String validity;
+  private final int port;
 
-    @Autowired
-    public ElectricCarApplication(final ApplicationEventPublisher applicationEventPublisher,
-                                  final HttpServer httpServer, final ArrowheadHandler onboardingHandler,
-                                  final HttpClient httpClient, @Value("${server.name}") final String commonName,
-                                  @Value("${server.rfid}") final String rfid,
-                                  @Qualifier("greenControl") final ControllableLed green,
-                                  @Qualifier("redControl") final ControllableLed red,
-                                  final GroveButtonObserver buttonObserver) throws IOException {
-        this.applicationEventPublisher = applicationEventPublisher;
-        this.httpServer = httpServer;
-        this.onboardingHandler = onboardingHandler;
-        this.httpClient = httpClient;
-        this.commonName = commonName;
-        this.rfid = rfid;
-        this.green = green;
-        this.red = red;
+  @Autowired
+  public ElectricCarApplication(final ApplicationEventPublisher applicationEventPublisher, final HttpServer httpServer,
+                                final ArrowheadHandler onboardingHandler, final HttpClient httpClient,
+                                @Value("${server.name}") final String commonName,
+                                @Value("${server.rfid}") final String rfid,
+                                @Qualifier("greenControl") final ControllableLed green,
+                                @Qualifier("redControl") final ControllableLed red,
+                                final GroveButtonObserver buttonObserver) throws IOException {
+    this.applicationEventPublisher = applicationEventPublisher;
+    this.httpServer = httpServer;
+    this.onboardingHandler = onboardingHandler;
+    this.httpClient = httpClient;
+    this.commonName = commonName;
+    this.rfid = rfid;
+    this.green = green;
+    this.red = red;
 
-        httpServer.configureName(commonName);
-        buttonObserver.setListener(this::toggleOnboarding);
+    httpServer.configureName(commonName);
+    buttonObserver.setListener(this::toggleOnboarding);
 
-        ipAddress = IpUtilities.getAddressString();
-        macAddress = IpUtilities.getMacAddress(ipAddress);
-        validity = Utilities.convertZonedDateTimeToUTCString(ZonedDateTime.now().plusDays(1));
-        port = httpServer.getPort();
+    ipAddress = IpUtilities.getAddressString();
+    macAddress = IpUtilities.getMacAddress(ipAddress);
+    validity = Utilities.convertZonedDateTimeToUTCString(ZonedDateTime.now().plusDays(1));
+    port = httpServer.getPort();
 
-        buttonObserver.start();
-        logger.info("Listening to button");
+    buttonObserver.start();
+    logger.info("Listening to button");
+  }
+
+  public void toggleOnboarding() {
+    logger.info("Button trigger detected");
+    if (onboarded.get()) {
+      performOffboarding();
+    } else {
+      performOnboarding();
     }
+  }
 
-    public void toggleOnboarding() {
-        logger.info("Button trigger detected");
+  @PreDestroy
+  public void performOffboarding() {
+    try {
+      httpServer.stop();
+      green.blink();
+      red.blink();
+
+      logger.info("Unregistering myself ...");
+
+      try {
         if (onboarded.get()) {
-            performOffboarding();
-        } else {
-            performOnboarding();
+          final UriComponents unregisterUri = findUri(Constants.SERVICE_STATION_UNREGISTER);
+          httpClient
+              .sendRequest(unregisterUri, HttpMethod.POST, UnregisterResponseDTO.class, new UnregisterRequestDTO(rfid));
         }
+      } catch (final Exception e) {
+        logger.debug("Unable to unregister rfid: {}", e.getMessage());
+      }
+
+      onboardingHandler.unregisterService(Constants.SERVICE_CAR_RFID, commonName, ipAddress, port);
+      onboardingHandler.unregisterSystem(commonName, ipAddress, port);
+      onboardingHandler.unregisterDevice(commonName, macAddress);
+
+    } catch (final Exception e) {
+      logger.warn("Offboarding issue: {}: {}", e.getClass().getSimpleName(), e.getMessage());
+    } finally {
+      onboarded.set(false);
+      stopStatusLeds();
+    }
+  }
+
+  public void performOnboarding() {
+    try {
+      green.blink();
+      red.blink();
+
+      logger.info("Start onboarding ...");
+      final var creationRequest = new CertificateCreationRequestDTO(commonName);
+      onboardingHandler.onboard(new OnboardingWithNameRequestDTO(creationRequest));
+
+      final String authInfo = onboardingHandler.getAuthInfo();
+
+      final var deviceRequest = getDeviceRegistryRequest(ipAddress, macAddress, validity, authInfo, creationRequest);
+      final var systemRequest = getSystemRegistryRequest(ipAddress, macAddress, port, validity, rfid, authInfo,
+                                                         creationRequest);
+      final var serviceRequest = getServiceRegistryRequest(ipAddress, port, validity, rfid, authInfo,
+                                                           Constants.OP_CAR_RFID_URI, Constants.SERVICE_CAR_RFID);
+      onboardingHandler.registerDevice(deviceRequest);
+      onboardingHandler.registerSystem(systemRequest);
+      onboardingHandler.registerService(serviceRequest);
+
+      final UriComponents registerUri = findUri(Constants.SERVICE_STATION_REGISTER);
+      httpClient.sendRequest(registerUri, HttpMethod.POST, RegisterResponseDTO.class, new RegisterRequestDTO(rfid));
+
+      red.turnOff();
+      onboarded.set(true);
+    } catch (final Exception e) {
+      logger.error("Onboarding issue: {}: {}", e.getClass().getSimpleName(), e.getMessage(), e);
+      performOffboarding();
     }
 
-    @PreDestroy
-    public void performOffboarding() {
-        try {
-            httpServer.stop();
-            green.blink();
-            red.blink();
-
-            logger.info("Unregistering myself ...");
-
-            try {
-                if (onboarded.get()) {
-                    final UriComponents unregisterUri = findUri(Constants.SERVICE_STATION_UNREGISTER);
-                    httpClient.sendRequest(unregisterUri, HttpMethod.POST, UnregisterResponseDTO.class,
-                                           new UnregisterRequestDTO(rfid));
-                }
-            } catch (final Exception e) {
-                logger.debug("Unable to unregister rfid: {}", e.getMessage());
-            }
-
-            onboardingHandler.unregisterService(Constants.SERVICE_CAR_RFID, commonName, ipAddress, port);
-            onboardingHandler.unregisterSystem(commonName, ipAddress, port);
-            onboardingHandler.unregisterDevice(commonName, macAddress);
-
-        } catch (final Exception e) {
-            logger.warn("Offboarding issue: {}: {}", e.getClass().getSimpleName(), e.getMessage());
-        } finally {
-            onboarded.set(false);
-            stopStatusLeds();
-        }
+    try {
+      applicationEventPublisher.publishEvent(new OnboardingFinishedEvent(this));
+    } catch (final Exception e) {
+      logger.warn("Publishing issue: {}: {}", e.getClass().getSimpleName(), e.getMessage(), e);
+      try {
+        startWebServer();
+      } catch (IOException ex) {
+        logger.error("Unable to start webserver: {}: {}", e.getClass().getSimpleName(), e.getMessage(), e);
+      }
     }
+  }
 
-    public void performOnboarding() {
-        try {
-            green.blink();
-            red.blink();
+  @EventListener(OnboardingFinishedEvent.class)
+  public void startWebServer() throws IOException {
+    httpServer.init();
+    httpServer.start();
+    green.turnOn();
+    logger.info("Status led turned on");
+  }
 
-            logger.info("Start onboarding ...");
-            final var creationRequest = new CertificateCreationRequestDTO(commonName);
-            onboardingHandler.onboard(new OnboardingWithNameRequestDTO(creationRequest));
+  @EventListener(OffboardingFinishedEvent.class)
+  public void stopStatusLeds() {
+    green.turnOff();
+    red.turnOff();
+    logger.info("Status led(s) turned off");
+  }
 
-            final String authInfo = onboardingHandler.getAuthInfo();
+  private DeviceRegistryOnboardingWithNameRequestDTO getDeviceRegistryRequest(String ipAddress, String macAddress,
+                                                                              String validity, String authInfo,
+                                                                              CertificateCreationRequestDTO creationRequest) {
 
-            final var deviceRequest = getDeviceRegistryRequest(ipAddress, macAddress, validity, authInfo,
-                                                               creationRequest);
-            final var systemRequest = getSystemRegistryRequest(ipAddress, macAddress, port, validity, rfid, authInfo,
-                                                               creationRequest);
-            final var serviceRequest = getServiceRegistryRequest(ipAddress, port, validity, rfid, authInfo,
-                                                                 Constants.OP_CAR_RFID_URI, Constants.SERVICE_CAR_RFID);
-            onboardingHandler.registerDevice(deviceRequest);
-            onboardingHandler.registerSystem(systemRequest);
-            onboardingHandler.registerService(serviceRequest);
+    return new DeviceRegistryOnboardingWithNameRequestDTO(getDeviceRequest(ipAddress, macAddress, authInfo), validity,
+                                                          null, null, creationRequest);
+  }
 
-            final UriComponents registerUri = findUri(Constants.SERVICE_STATION_REGISTER);
-            httpClient
-                .sendRequest(registerUri, HttpMethod.POST, RegisterResponseDTO.class, new RegisterRequestDTO(rfid));
+  private SystemRegistryOnboardingWithNameRequestDTO getSystemRegistryRequest(String ipAddress, String macAddress,
+                                                                              final int port, String validity,
+                                                                              String rfid, String authInfo,
+                                                                              CertificateCreationRequestDTO creationRequest) {
+    final Map<String, String> metadata = new HashMap<>();
+    metadata.put("rfid", rfid);
+    return new SystemRegistryOnboardingWithNameRequestDTO(getSystemRequest(ipAddress, port, authInfo),
+                                                          getDeviceRequest(ipAddress, macAddress, authInfo), validity,
+                                                          metadata, null, creationRequest);
+  }
 
-            applicationEventPublisher.publishEvent(new OnboardingFinishedEvent(this));
-            red.turnOff();
-            onboarded.set(true);
-        } catch (final Exception e) {
-            logger.warn("Onboarding issue: {}: {}", e.getClass().getSimpleName(), e.getMessage());
-            performOffboarding();
-        }
-    }
+  private ServiceRegistryRequestDTO getServiceRegistryRequest(String ipAddress, final int port, final String validity,
+                                                              final String rfid, final String authInfo,
+                                                              final String uriPostfix, final String serviceDef) {
+    final ServiceRegistryRequestDTO requestDTO = new ServiceRegistryRequestDTO();
+    requestDTO.setSecure(ServiceSecurityType.CERTIFICATE.name());
+    requestDTO.setServiceUri(Constants.CAR_CONTROLLER_PATH + uriPostfix);
+    requestDTO.setInterfaces(List.of(CommonConstants.HTTP_SECURE_JSON));
+    requestDTO.setEndOfValidity(validity);
+    requestDTO.setProviderSystem(getSystemRequest(ipAddress, port, authInfo));
+    requestDTO.setServiceDefinition(serviceDef);
+    requestDTO.setMetadata(Map.of("rfid", rfid));
+    return requestDTO;
+  }
 
-    @EventListener(OnboardingFinishedEvent.class)
-    public void startWebServer() throws IOException {
-        httpServer.init();
-        httpServer.start();
-        green.turnOn();
-        logger.info("Status led turned on");
-    }
+  private UriComponents findUri(final String serviceDef) {
+    final ServiceQueryFormDTO serviceQueryFormDTO = new ServiceQueryFormDTO.Builder(serviceDef)
+        .interfaces(CommonConstants.HTTP_SECURE_JSON).build();
 
-    @EventListener(OffboardingFinishedEvent.class)
-    public void stopStatusLeds() {
-        green.turnOff();
-        red.turnOff();
-        logger.info("Status led(s) turned off");
-    }
+    final String authInfo = onboardingHandler.getAuthInfo();
+    final SystemRequestDTO systemRequest = getSystemRequest(ipAddress, port, authInfo);
 
-    private DeviceRegistryOnboardingWithNameRequestDTO getDeviceRegistryRequest(String ipAddress, String macAddress,
-                                                                                String validity, String authInfo,
-                                                                                CertificateCreationRequestDTO creationRequest) {
+    final OrchestrationResponseDTO orchestrationResponseDTO = onboardingHandler
+        .lookupOrchestration(serviceQueryFormDTO, systemRequest);
 
-        return new DeviceRegistryOnboardingWithNameRequestDTO(getDeviceRequest(ipAddress, macAddress, authInfo),
-                                                              validity, null, null, creationRequest);
-    }
+    return onboardingHandler.createUri(orchestrationResponseDTO.getResponse().get(0));
+  }
 
-    private SystemRegistryOnboardingWithNameRequestDTO getSystemRegistryRequest(String ipAddress, String macAddress,
-                                                                                final int port, String validity,
-                                                                                String rfid, String authInfo,
-                                                                                CertificateCreationRequestDTO creationRequest) {
-        final Map<String, String> metadata = new HashMap<>();
-        metadata.put("rfid", rfid);
-        return new SystemRegistryOnboardingWithNameRequestDTO(getSystemRequest(ipAddress, port, authInfo),
-                                                              getDeviceRequest(ipAddress, macAddress, authInfo),
-                                                              validity, metadata, null, creationRequest);
-    }
+  private DeviceRequestDTO getDeviceRequest(String ipAddress, String macAddress, String authInfo) {
+    return new DeviceRequestDTO(commonName, ipAddress, macAddress, authInfo);
+  }
 
-    private ServiceRegistryRequestDTO getServiceRegistryRequest(String ipAddress, final int port, final String validity,
-                                                                final String rfid, final String authInfo,
-                                                                final String uriPostfix, final String serviceDef) {
-        final ServiceRegistryRequestDTO requestDTO = new ServiceRegistryRequestDTO();
-        requestDTO.setSecure(ServiceSecurityType.CERTIFICATE.name());
-        requestDTO.setServiceUri(Constants.CAR_CONTROLLER_PATH + uriPostfix);
-        requestDTO.setInterfaces(List.of(CommonConstants.HTTP_SECURE_JSON));
-        requestDTO.setEndOfValidity(validity);
-        requestDTO.setProviderSystem(getSystemRequest(ipAddress, port, authInfo));
-        requestDTO.setServiceDefinition(serviceDef);
-        requestDTO.setMetadata(Map.of("rfid", rfid));
-        return requestDTO;
-    }
-
-    private UriComponents findUri(final String serviceDef) {
-        final ServiceQueryFormDTO serviceQueryFormDTO = new ServiceQueryFormDTO.Builder(serviceDef)
-            .interfaces(CommonConstants.HTTP_SECURE_JSON).build();
-
-        final String authInfo = onboardingHandler.getAuthInfo();
-        final SystemRequestDTO systemRequest = getSystemRequest(ipAddress, port, authInfo);
-
-        final OrchestrationResponseDTO orchestrationResponseDTO = onboardingHandler
-            .lookupOrchestration(serviceQueryFormDTO, systemRequest);
-
-        return onboardingHandler.createUri(orchestrationResponseDTO.getResponse().get(0));
-    }
-
-    private DeviceRequestDTO getDeviceRequest(String ipAddress, String macAddress, String authInfo) {
-        return new DeviceRequestDTO(commonName, ipAddress, macAddress, authInfo);
-    }
-
-    private SystemRequestDTO getSystemRequest(final String ipAddress, final int port, final String authInfo) {
-        return new SystemRequestDTO(commonName, ipAddress, port, authInfo);
-    }
+  private SystemRequestDTO getSystemRequest(final String ipAddress, final int port, final String authInfo) {
+    return new SystemRequestDTO(commonName, ipAddress, port, authInfo);
+  }
 
 }
